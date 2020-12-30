ad78ab65964d62852814fbfe193615583e141f8a
Songwhan Han <hansh1029@gmail.com>
2020-12-30 14:38:04 +0900

    other controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import routes from "../routes";
 
 export const getJoin = (req, res) => {
-  res.render("Join", { pageTitle: "Join" });
+  res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
@@ -10,7 +10,7 @@ export const postJoin = (req, res) => {
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render("Join", { pageTitle: "Join" });
+    res.render("join", { pageTitle: "Join" });
   } else {
     //Todo register user
     //Todo Log user in
@@ -19,21 +19,23 @@ export const postJoin = (req, res) => {
 };
 
 export const getLogin = (req, res) => {
-    res.render("Login", { pageTitle: "Login" })
+  res.render("login", { pageTitle: "Login" });
 };
 
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
+  res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render("Logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
 
 export const userDetail = (req, res) =>
-  res.render("UserDetail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 
 export const editProfile = (req, res) =>
-  res.render("EditProfile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 
 export const changePassword = (req, res) =>
-  res.render("ChangePassword", { pageTitle: "Change Password" });
+  res.render("changePassword", { pageTitle: "Change Password" });
