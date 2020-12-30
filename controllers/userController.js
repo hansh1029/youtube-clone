d8adfc7790f46803b6502af4f0f7f94be0ad69ff
Songwhan Han <hansh1029@gmail.com>
2020-12-30 13:50:00 +0900

    join controller: add postJoin, getJoin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("Join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("Join", { pageTitle: "Join" });
+  } else {
+    //Todo register user
+    //Todo Log user in
+    res.redirect(routes.home);
+  }
+};
 
 export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
 
