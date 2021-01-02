68713dd3e00a7e266977270cf9d90b2258640d5e
Songwhan Han <hansh1029@gmail.com>
2021-01-03 08:57:01 +0900

    add comment with ajax in front side

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
 import "../scss/styles.scss";
-
-const videoContainer = document.querySelector(".video-detail__container");
-
-const registerView = () => {
-  const videoId = window.location.href.split("/videos/")[1];
-  fetch(`/api/${videoId}/view`, {
-    method: "POST",
-  });
-};
-
-function init() {
-  registerView();
-}
-
-if (videoContainer) {
-  init();
-}
+import "./registerView";
+import "./addComment";
---
+++ assets/js/registerView.js
@@ -0,0 +1,16 @@
+const videoContainer = document.querySelector(".video-detail__container");
+
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1];
+  fetch(`/api/${videoId}/view`, {
+    method: "POST",
+  });
+};
+
+function init() {
+  registerView();
+}
+
+if (videoContainer) {
+  init();
+}
