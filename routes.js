aea95a0ada334bba845053f535d365ec7e24ccca
Songwhan Han <hansh1029@gmail.com>
2021-01-03 08:45:06 +0900

    Add comment api backend side

--- routes.js
+++ routes.js
@@ -25,6 +25,7 @@ const DELETE_VIDEO = "/:id/delete";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -68,6 +69,7 @@ const routes = {
   },
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
