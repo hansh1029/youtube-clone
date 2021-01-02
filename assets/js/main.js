86f8bec51b0cd97434b311615b1a77203b9de4de
Songwhan Han <hansh1029@gmail.com>
2021-01-02 21:26:52 +0900

    call register view api with ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 import "../scss/styles.scss";
 
+const videoContainer = document.querySelector(".video-detail__container");
+
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1];
+  fetch(`/api/${videoId}/view`, {
+    method: "POST",
+  });
+  console.log("add view");
+};
+
+function init() {
+  registerView();
+}
+
+if (videoContainer) {
+  init();
+}
